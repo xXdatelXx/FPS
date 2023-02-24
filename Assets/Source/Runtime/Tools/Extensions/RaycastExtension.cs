ca7bfd1f36c7b34de9c46e286a04e8660c18426a
xXdatelXx <104914667+xXdatelXx@users.noreply.github.com>
2023-02-24 08:37:32 +0200

    Optimized RaycastExtension algorithm

--- Assets/Source/Runtime/Tools/Extensions/RaycastExtension.cs
+++ Assets/Source/Runtime/Tools/Extensions/RaycastExtension.cs
@@ -6,17 +6,13 @@ namespace Source.Runtime.Tools.Extensions
     public static class RaycastExtension
     {
         public static bool Is<T>(this RaycastHit hit) => hit.collider.Is<T>();
-        
+
         public static bool Is<T>(this RaycastHit hit, out IRayData<T> data)
         {
-            if (hit.collider.Is(out T target))
-            {
-                data = new RayData<T>(hit.point, hit.distance, target);
-                return true;
-            }
-
-            data = default;
-            return false;
+            var targetHit = hit.collider.Is(out T target);
+            data = targetHit ? new RayData<T>(hit.point, hit.distance, target) : default;
+            
+            return targetHit;
         }
     }
 }
