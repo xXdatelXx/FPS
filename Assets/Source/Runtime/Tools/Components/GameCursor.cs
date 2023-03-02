ae8823237f14f349785ca866f5475217a95a9472
xXdatelXx <104914667+xXdatelXx@users.noreply.github.com>
2023-03-02 18:57:25 +0200

    Added GameCursor

---
+++ Assets/Source/Runtime/Tools/Components/GameCursor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace FPS.Tools
+{
+    public sealed class GameCursor : MonoBehaviour
+    {
+        private void Awake()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+}
