705336a23f6df289a18f0b7ca87c755dea472373
xXdatelXx <104914667+xXdatelXx@users.noreply.github.com>
2023-03-06 11:12:21 +0200

    Added GameLoopTest

---
+++ Assets/Source/Tests/Game/GameLoopTest.cs
@@ -0,0 +1,39 @@
+﻿using Cysharp.Threading.Tasks;
+using FPS.Game;
+using NUnit.Framework;
+
+namespace Source.Tests.Game
+{
+    public sealed class GameLoopTest
+    {
+        private readonly IGameLoop _loop;
+        private readonly TickMarker _marker;
+
+        public GameLoopTest()
+        {
+            _loop = new GameLoop(new GameTime());
+            _marker = new TickMarker();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            _loop.Add(_marker);
+            _loop.Start();
+        }
+        
+        [Test]
+        public async void TickingCorrectly()
+        {
+            await UniTask.Yield();
+            Assert.AreEqual(_marker.WasTick, true);
+        }
+
+        [Test]
+        public async void CorrectlyDeltaTime()
+        {
+            await UniTask.Yield();
+            Assert.AreEqual(_marker.DeltaTime > 0, true);   
+        }
+    }
+}
