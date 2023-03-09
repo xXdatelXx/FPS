49e71f66870a71832823a322670f0162cee69b24
xXdatelXx <104914667+xXdatelXx@users.noreply.github.com>
2023-03-09 20:21:44 +0200

    Added sprite system

---
+++ Assets/Source/Runtime/Tools/Components/Sprite/UnitySpite.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace FPS.Tools
+{
+    public sealed class UnitySpite : ISpite
+    {
+        private readonly SpriteRenderer _spriteRenderer;
+        private readonly Sprite _sprite;
+
+        public UnitySpite(SpriteRenderer spriteRenderer, Sprite sprite)
+        {
+            _spriteRenderer = spriteRenderer.ThrowExceptionIfArgumentNull(nameof(spriteRenderer));
+            _sprite = sprite.ThrowExceptionIfArgumentNull(nameof(sprite));
+        }
+
+        public void Render() => 
+            _spriteRenderer.sprite = _sprite;
+
+        public void Hide() => 
+            _spriteRenderer.sprite = default;
+    }
+}
