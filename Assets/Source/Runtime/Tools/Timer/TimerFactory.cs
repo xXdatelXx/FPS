8f9e23b8bcc329012413c9217de8bcb8ff8427a0
xXdatelXx <104914667+xXdatelXx@users.noreply.github.com>
2023-03-16 22:50:46 +0200

    Added timer factory

---
+++ Assets/Source/Runtime/Tools/Timer/TimerFactory.cs
@@ -0,0 +1,12 @@
+﻿namespace FPS.Tools
+{
+    public sealed class TimerFactory : ITimerFactory
+    {
+        private readonly float _time;
+
+        public TimerFactory(float time) =>
+            _time = time.ThrowExceptionIfValueSubZero(nameof(time));
+
+        public ITimer Create() => new Timer(_time);
+    }
+}
