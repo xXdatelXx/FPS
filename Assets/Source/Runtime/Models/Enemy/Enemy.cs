359e2f22195895ce2847fd07761df3a08ecdaf60
xXdatelXx <104914667+xXdatelXx@users.noreply.github.com>
2023-03-16 22:53:15 +0200

    Added test enemy

---
+++ Assets/Source/Runtime/Models/Enemy/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace FPS.Model
+{
+    // For test
+    [RequireComponent(typeof(Collider))]
+    public sealed class Enemy : MonoBehaviour, IHealth
+    {
+        [SerializeField] private UnityEvent _onDie;
+        private IHealth _health;
+        public bool Died => _health.Died;
+
+        private void Awake() => _health = new Health(100);
+
+        public void TakeDamage(float damage)
+        {
+            _health.TakeDamage(damage);
+
+            if (Died)
+                _onDie.Invoke();
+        }
+    }
+}
