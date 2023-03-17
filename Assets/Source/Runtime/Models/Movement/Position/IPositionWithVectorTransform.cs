3d4cb996e1350fe04a9e917725af53ea703e828c
xXdatelXx <104914667+xXdatelXx@users.noreply.github.com>
2023-03-17 21:55:37 +0200

    Added new position inheritor

---
+++ Assets/Source/Runtime/Models/Movement/Position/IPositionWithVectorTransform.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace FPS.Model
+{
+    public interface IPositionWithVectorTransform : IPosition
+    {
+        Vector3 TransformVector(Vector3 vector);
+        Vector3 TransformPoint(Vector3 vector);
+        Vector3 TransformDirection(Vector3 vector);
+    }
+}
