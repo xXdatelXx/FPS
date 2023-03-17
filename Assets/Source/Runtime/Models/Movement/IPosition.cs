678104ca0a3340bc22bca57e7d446605e25b0e40
xXdatelXx <104914667+xXdatelXx@users.noreply.github.com>
2023-03-17 09:53:13 +0200

    Added IPosition interface

---
+++ Assets/Source/Runtime/Models/Movement/IPosition.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace FPS.Model
+{
+    public interface IPosition
+    {
+        Vector3 World { get; }
+        Vector3 Local { get; }
+    }
+}
---
+++ Assets/Source/Runtime/Models/Movement/Position.cs
@@ -0,0 +1,16 @@
+﻿using FPS.Tools;
+using UnityEngine;
+
+namespace FPS.Model
+{
+    public readonly struct Position : IPosition
+    {
+        private readonly Transform _transform;
+
+        public Position(Transform transform) => 
+            _transform = transform.ThrowExceptionIfArgumentNull(nameof(transform));
+
+        public Vector3 World => _transform.position;
+        public Vector3 Local => _transform.localPosition;
+    }
+}
