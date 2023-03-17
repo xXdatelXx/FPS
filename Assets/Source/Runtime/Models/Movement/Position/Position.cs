3d6c000c2a0b178871b117de0d2ed9517cc8c468
xXdatelXx <104914667+xXdatelXx@users.noreply.github.com>
2023-03-17 21:55:47 +0200

    Added new position inheritor

---
+++ Assets/Source/Runtime/Models/Movement/Position/Position.cs
@@ -0,0 +1,25 @@
+﻿using FPS.Tools;
+using UnityEngine;
+
+namespace FPS.Model
+{
+    public readonly struct Position : IPositionWithVectorTransform
+    {
+        private readonly Transform _transform;
+
+        public Position(Transform transform) => 
+            _transform = transform.ThrowExceptionIfArgumentNull(nameof(transform));
+
+        public Vector3 World => _transform.position;
+        public Vector3 Local => _transform.localPosition;
+        
+        public Vector3 TransformVector(Vector3 vector) => 
+            _transform.TransformVector(vector);
+
+        public Vector3 TransformPoint(Vector3 vector) => 
+            _transform.TransformPoint(vector);
+
+        public Vector3 TransformDirection(Vector3 vector) => 
+            _transform.TransformDirection(vector);
+    }
+}
