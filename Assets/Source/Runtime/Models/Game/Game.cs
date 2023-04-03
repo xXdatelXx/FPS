07d3f6cf71aa438a592daa6adad7a35e7c4a193c
xXdatelXx <104914667+xXdatelXx@users.noreply.github.com>
2023-04-03 19:44:43 +0200

    Reworked Game.cs

--- Assets/Source/Runtime/Models/Game/Game.cs
+++ Assets/Source/Runtime/Models/Game/Game.cs
@@ -7,22 +7,18 @@ namespace FPS.Game
     public sealed class Game : IGame
     {
         private readonly IGameLoop _gameLoop;
-        private readonly IPlayer _player;
-        private readonly IPlayerWithWeapons _weapons;
 
         public Game(IGameEngine engine)
         {
             var time = new GameTime();
-            _player = engine.Factories.PlayerFactory.Create(time);
-            _weapons = engine.Factories.PlayerWeaponFactory.Create();
+            var player = engine.Factories.PlayerFactory.Create(time);
+            var weapons = engine.Factories.PlayerWeaponFactory.Create();
+            
             _gameLoop = new GameLoop(time);
+            _gameLoop.Add(player);
+            _gameLoop.Add(weapons);
         }
 
-        public void Play()
-        {
-            _gameLoop.Add(_player);
-            _gameLoop.Add(_weapons);
-            _gameLoop.Start();
-        }
+        public void Play() => _gameLoop.Start();
     }
 }
