27d808f6dd6619e0b4b54d064753eaaf2105feba
xXdatelXx <104914667+xXdatelXx@users.noreply.github.com>
2023-04-04 16:06:54 +0200

    Added gameLoop extension

---
+++ Assets/Source/Runtime/Tools/Extensions/GameLoopExtension.cs
@@ -0,0 +1,25 @@
+﻿using FPS.Tools.GameLoop;
+
+namespace FPS.Tools
+{
+    public static class GameLoopExtension
+    {
+        public static void Add(this IReadOnlyGameLoop loop, params ITickable[] tickables)
+        {
+            foreach (var i in tickables) 
+                loop.Add(i);
+        }
+        
+        public static void Add(this IReadOnlyGameLoop loop, params IFixedTickable[] tickables)
+        {
+            foreach (var i in tickables) 
+                loop.Add(i);
+        }
+        
+        public static void Add(this IReadOnlyGameLoop loop, params ILateTickable[] tickables)
+        {
+            foreach (var i in tickables) 
+                loop.Add(i);
+        }
+    }
+}
