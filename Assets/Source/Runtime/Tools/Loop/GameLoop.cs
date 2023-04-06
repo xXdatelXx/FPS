9b9212460a6c14b06ded7dabbfe01b16e532a551
xXdatelXx <104914667+xXdatelXx@users.noreply.github.com>
2023-04-06 23:15:52 +0200

    Added precondition for GameLoop.cs

--- Assets/Source/Runtime/Tools/Loop/GameLoop.cs
+++ Assets/Source/Runtime/Tools/Loop/GameLoop.cs
@@ -6,31 +6,50 @@ namespace FPS.Tools.GameLoop
 {
     public sealed class GameLoop : IGameLoop
     {
-        private readonly List<IFixedTickable> _fixedTickables = new();
-        private readonly List<ILateTickable> _lateTickables = new();
-        private readonly List<ITickable> _tickables = new();
+        private readonly List<IGameLoopObject> _objects = new();
+        private readonly List<IFixedGameLoopObject> _fixedObjects = new();
+        private readonly List<ILateGameLoopObject> _lateObjects = new();
         private readonly IReadOnlyGameTime _time;
 
         public GameLoop(IReadOnlyGameTime time) =>
             _time = time.ThrowExceptionIfArgumentNull(nameof(time));
 
-        public void Add(ITickable tickable) =>
-            _tickables.Add(tickable.ThrowExceptionIfArgumentNull(nameof(tickable)));
+        #region Add methods
 
-        public void Add(ILateTickable tickable) =>
-            _lateTickables.Add(tickable.ThrowExceptionIfArgumentNull(nameof(tickable)));
+        public void Add(IGameLoopObject obj)
+        {
+            if (_objects.Has(obj))
+                throw new InvalidOperationException(nameof(Add));
 
-        public void Add(IFixedTickable tickable) =>
-            _fixedTickables.Add(tickable.ThrowExceptionIfArgumentNull(nameof(tickable)));
+            _objects.Add(obj.ThrowExceptionIfArgumentNull(nameof(obj)));
+        }
+
+        public void Add(ILateGameLoopObject obj)
+        {
+            if (_lateObjects.Has(obj))
+                throw new InvalidOperationException(nameof(Add));
+
+            _lateObjects.Add(obj.ThrowExceptionIfArgumentNull(nameof(obj)));
+        }
+
+        public void Add(IFixedGameLoopObject obj)
+        {
+            if (_fixedObjects.Has(obj))
+                throw new InvalidOperationException(nameof(Add));
+
+            _fixedObjects.Add(obj.ThrowExceptionIfArgumentNull(nameof(obj)));
+        }
+
+        #endregion
 
         public void Start()
         {
             Start(UniTask.Yield(),
-                () => _tickables.ForEach(i => i.Tick(_time.Delta)));
+                () => _objects.ForEach(i => i.Tick(_time.Delta)));
             Start(UniTask.Yield(PlayerLoopTiming.PostLateUpdate),
-                () => _lateTickables.ForEach(i => i.LateTick()));
+                () => _lateObjects.ForEach(i => i.LateTick()));
             Start(UniTask.WaitForFixedUpdate(),
-                () => _fixedTickables.ForEach(i => i.FixedTick(_time.FixedDelta)));
+                () => _fixedObjects.ForEach(i => i.FixedTick(_time.FixedDelta)));
         }
 
         private async void Start(YieldAwaitable yieldAwaitable, Action updateAction)
