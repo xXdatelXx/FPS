c0b8c9636f6753792d3d29e6b2362f0bae2adae4
xXdatelXx <96194749+Datela@users.noreply.github.com>
2023-06-16 20:33:23 +0200

    Added vector random

---
+++ Assets/Source/Toolkit/Random/RationalRandom.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace FPS.Toolkit
+{
+    public sealed class RationalRandom : IRandom<float>
+    {
+        private readonly Range _range;
+
+        public RationalRandom(float min, float max) : this(new Range(min, max))
+        { }
+
+        public RationalRandom(Range range) => _range = range;
+
+        public float Next() => Random.Range(_range.Min, _range.Max);
+    }
+}
