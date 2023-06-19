a24fadade5af7bfd46a2649813797ad7a9a2cd5e
xXdatelXx <104914667+xXdatelXx@users.noreply.github.com>
2023-06-19 16:06:37 +0200

    Custom exception become serializable

--- Assets/Source/Toolkit/Exceptions/SubOrEqualZeroException.cs
+++ Assets/Source/Toolkit/Exceptions/SubOrEqualZeroException.cs
@@ -2,6 +2,7 @@ using System;
 
 namespace FPS.Toolkit
 {
+    [Serializable]
     public sealed class SubOrEqualZeroException : Exception
     {
         public SubOrEqualZeroException(string message) : base($"{message} <= 0")
--- Assets/Source/Toolkit/Exceptions/SubZeroException.cs
+++ Assets/Source/Toolkit/Exceptions/SubZeroException.cs
@@ -2,6 +2,7 @@ using System;
 
 namespace FPS.Toolkit
 {
+    [Serializable]
     public sealed class SubZeroException : Exception
     {
         public SubZeroException(string message) : base($"{message} < 0")
