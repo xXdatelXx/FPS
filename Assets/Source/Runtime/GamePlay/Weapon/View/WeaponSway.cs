6d8c6e879178b7bf48fe1373230a3cba4ea5937f
xXdatelXx <104914667+xXdatelXx@users.noreply.github.com>
2023-06-19 14:22:43 +0200

    Renamed fields in WeaponSway.cs

--- Assets/Source/Runtime/GamePlay/Weapon/View/WeaponSway.cs
+++ Assets/Source/Runtime/GamePlay/Weapon/View/WeaponSway.cs
@@ -3,18 +3,15 @@ using UnityEngine;
 
 namespace FPS.Model
 {
-    //TODO Composition > Aggregation (input)
-    /// <summary>
-    /// Makes the weapon not strictly attached to the camera
-    /// </summary>
     public sealed class WeaponSway : MonoBehaviour
     {
         [SerializeField] private float _speed;
-        [SerializeField] private float _amount;
-        [SerializeField] private float _maxAmount;
+        [SerializeField] private float _force;
+        [SerializeField] private float _limit;
         private IPlayerMovementInput _input;
 
-        private void Awake() => _input = new PlayerMovementInput();
+        private void Awake() => 
+            _input = new PlayerMovementInput();
 
         private void Update() =>
             transform.localPosition =
@@ -23,7 +20,7 @@ namespace FPS.Model
         private Vector2 Rotation()
         {
             var rotation = _input.Rotation();
-            float Clamp(float angle) => Mathf.Clamp(angle * _amount, -_maxAmount, _maxAmount);
+            float Clamp(float angle) => Mathf.Clamp(angle * _force, -_limit, _limit);
 
             return new(-Clamp(rotation.y), Clamp(rotation.x));
         }
