094c24be75051d6034f8f6316bd7d817c577680f
xXdatelXx <104914667+xXdatelXx@users.noreply.github.com>
2023-06-20 10:58:02 +0200

    Added storage extensions

---
+++ Assets/Source/Toolkit/Extensions/StorageExtension.cs
@@ -0,0 +1,13 @@
+﻿using FPS.Toolkit.Storage;
+
+namespace FPS.Toolkit
+{
+    public static class StorageExtension
+    {
+        public static bool NullOrDefault<T>(this IStorage<T> storage) =>
+            !storage.Exists || storage.Load().Equals(default(T));
+
+        public static T LoadOrDefault<T>(this IStorage<T> storage) => 
+            storage.Exists ? storage.Load() : default;
+    }
+}
