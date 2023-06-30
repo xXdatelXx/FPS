24a7b249445cbdcbaed0dfab001312451e434c01
xXdatelXx <104914667+xXdatelXx@users.noreply.github.com>
2023-06-30 14:16:04 +0200

    Updated CollectionExtension.cs

--- Assets/Source/Toolkit/Extensions/CollectionExtension.cs
+++ Assets/Source/Toolkit/Extensions/CollectionExtension.cs
@@ -10,5 +10,13 @@ namespace FPS.Toolkit
 
         public static IEnumerable<T> TryThrowNullReferenceForeach<T>(this IEnumerable<T> enumerable, string name) =>
             enumerable.Select(i => i.ThrowExceptionIfNull(name));
+        
+        public static T RandomElement<T>(this IEnumerable<T> enumerable)
+        {
+            var array = enumerable as T[] ?? enumerable.ToArray();
+            var index = new NumberRandom(0, array.Length).Next();
+            
+            return array.ElementAt(index);
+        }
     }
 }
