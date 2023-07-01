3ea14f50648703efefc56f5fcec3f2ec849e120c
xXdatelXx <104914667+xXdatelXx@users.noreply.github.com>
2023-07-01 22:31:32 +0200

    Added nav mesh movement

---
+++ Assets/Source/Toolkit/Transformation/Movement/NavMeshMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace FPS.Toolkit
+{
+    public sealed class NavMeshMovement : IMovement
+    {
+        private readonly NavMeshAgent _agent;
+
+        public NavMeshMovement(NavMeshAgent agent)
+        {
+            _agent = agent.ThrowExceptionIfArgumentNull(nameof(agent));
+            Position = new Position(_agent.transform);
+        }
+
+        public IReadOnlyPosition Position { get; }
+        
+        public void Move(Vector3 motion) => _agent.Move(motion);
+    }
+}
