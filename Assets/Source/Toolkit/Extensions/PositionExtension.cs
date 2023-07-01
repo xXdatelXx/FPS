fac661c318a7e399d2ba4f605a14149cefd51f8b
xXdatelXx <104914667+xXdatelXx@users.noreply.github.com>
2023-07-01 22:34:51 +0200

    Added position extension

---
+++ Assets/Source/Toolkit/Extensions/PositionExtension.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace FPS.Toolkit
+{
+    public static class PositionExtension
+    {
+        public static bool IsSame(this IReadOnlyPosition a, IReadOnlyPosition b) =>
+            a.Value == b.Value;
+
+        public static float Distance(this IReadOnlyPosition a, IReadOnlyPosition b) =>
+            Distance(a, b.Value);
+
+        public static float Distance(this IReadOnlyPosition a, Vector3 b)
+            => Vector3.Distance(a.Value, b);
+    }
+}
