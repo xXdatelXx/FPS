16567e1fae2448d8997e72273fe3711789ccf755
xXdatelXx <104914667+xXdatelXx@users.noreply.github.com>
2023-07-06 23:54:37 +0200

    Updated enemy directory

---
+++ Assets/Source/Runtime/GamePlay/Enemy/Factories/EnemyFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using FPS.Toolkit;
+using UnityEngine;
+
+namespace FPS.GamePlay
+{
+    public sealed class EnemyFactory : IFactory<Enemy>
+    {
+        private readonly Enemy _prefab;
+        private readonly ICharacter _character;
+        private readonly IHealth _health;
+
+        public EnemyFactory(Enemy prefab, ICharacter character)
+        {
+            _prefab = prefab.ThrowExceptionIfArgumentNull(nameof(prefab));
+            _character = character.ThrowExceptionIfArgumentNull(nameof(character));
+        }
+
+        public Enemy Create()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
