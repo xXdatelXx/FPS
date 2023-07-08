a1d3b7001b7230111b382ba65ecf6e4c0b992f9c
xXdatelXx <104914667+xXdatelXx@users.noreply.github.com>
2023-07-09 00:02:30 +0200

    Added RandomNegative.cs

---
+++ Assets/Source/Toolkit/Random/RandomNegative.cs
@@ -0,0 +1,29 @@
+﻿    using System;
+
+namespace FPS.Toolkit
+{
+    public sealed class RandomNegative<T> : IRandom<T> where T : IComparable
+    {
+        private readonly IRandom<T> _random;
+        private readonly IChance _negateChance;
+
+        public RandomNegative(IRandom<T> random, IChance negateChance)
+        {
+            _random = random.ThrowExceptionIfArgumentNull(nameof(random));
+            _negateChance = negateChance.ThrowExceptionIfArgumentNull(nameof(negateChance));
+        }
+
+        public T Next()
+        {
+            var next = _random.Next();
+
+            if (_negateChance.TryLuck())
+            {
+                dynamic negativeOne = -1;
+                next *= negativeOne;
+            }
+
+            return next;
+        }
+    }
+}
