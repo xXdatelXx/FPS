1d8ab8b721d4ff86d9ed1f7fb2129fc1d87792ec
xXdatelXx <104914667+xXdatelXx@users.noreply.github.com>
2023-07-10 14:20:04 +0200

    Added enemy health view

---
+++ Assets/Source/Runtime/GamePlay/Enemy/Model/View/EnemyHealthView.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace FPS.GamePlay
+{
+    [RequireComponent(typeof(Animator))]
+    public sealed class EnemyHealthView : MonoBehaviour, IHealthView
+    {
+        [SerializeField] private string _dieAnimation;
+        [SerializeField, Range(0, 10)] private float _dieTime;
+        private Animator _animator;
+        
+        public void Visualize(float health)
+        {
+        }
+
+        public void Die()
+        {
+            _animator.Play(_dieAnimation);
+            Destroy(gameObject, _dieTime);
+        }
+    }
+}
