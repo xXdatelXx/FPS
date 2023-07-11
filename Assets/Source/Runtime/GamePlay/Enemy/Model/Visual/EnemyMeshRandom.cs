aecebded9958a4eb45a1f5b2929dcbcc948576a0
xXdatelXx <104914667+xXdatelXx@users.noreply.github.com>
2023-07-11 20:08:30 +0200

    Added enemy mesh random

---
+++ Assets/Source/Runtime/GamePlay/Enemy/Model/Visual/EnemyMeshRandom.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FPS.Toolkit;
+using UnityEngine;
+
+namespace FPS.GamePlay
+{
+    public sealed class EnemyMeshRandom : MonoBehaviour
+    {
+        [SerializeField] private List<MeshFilter> _organs;
+        [SerializeField] private List<List<Mesh>> _organsMeshes;
+
+        private void OnValidate()
+        {
+            if (_organsMeshes.Any(organs => organs.Count != _organs.Count))
+                throw new ArgumentOutOfRangeException(nameof(_organsMeshes));
+        }
+
+        private void Awake()
+        {
+            var meshes = _organsMeshes.RandomElement();
+            for (var i = 0; i < meshes.Count; i++) 
+                _organs[i].mesh = meshes[i];
+        }
+    }
+}
