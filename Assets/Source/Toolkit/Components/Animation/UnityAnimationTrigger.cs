0ceb3ffca565189f2aad8f1d39be35eebc865f24
HAY <artemgurv@gmail.com>
2023-10-08 21:19:49 +0200

    Added UnityAnimationtrigger

---
+++ Assets/Source/Toolkit/Components/Animation/UnityAnimationTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace FPS.Toolkit
+{
+    public sealed class UnityAnimationTrigger : IAnimation
+    {
+        private readonly Animator _animator;
+        private readonly string _trigger;
+
+        public UnityAnimationTrigger(Animator animator, string trigger)
+        {
+            _animator = animator.ThrowExceptionIfArgumentNull(nameof(animator));
+            _trigger = trigger.ThrowExceptionIfArgumentNull(nameof(trigger));
+        }
+
+        public void Play() => _animator.SetTrigger(_trigger);
+    }
+}
