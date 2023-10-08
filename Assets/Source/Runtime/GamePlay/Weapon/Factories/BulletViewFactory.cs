712e922fc82f24ad5129686d78aa89d6ec981bcd
xXdatelXx <artemgurv@gmail.com>
2023-10-08 21:38:01 +0200

    Revert "Updated bullets view"
    This reverts commit 8bb7e928ce684349f07a3d1a25a521107d74f03b.

--- Assets/Source/Runtime/GamePlay/Weapon/Factories/BulletViewFactory.cs
+++ Assets/Source/Runtime/GamePlay/Weapon/Factories/BulletViewFactory.cs
@@ -20,15 +20,8 @@ namespace FPS.Factories
             new GameLoop(new GameTime(), _rays).Start();
         }
 
-        public IBulletView Create()
-        {
-            return new BulletViewSequence
-            (
-     new BulletViewWithCrosshair(_crosshairFactory.Create()),
-                new BulletViewWithTrace(CreateRays()),
-                new BulletViewWithParticles(new BulletParticle(_startBulletParticle), CreateHitsView())
-            );
-        }
+        public IBulletView Create() =>
+            new BulletView(new BulletParticle(_startBulletParticle), CreateHitsView(), CreateRays(), _crosshairFactory.Create());
 
         private IBulletTrace CreateRays() =>
             new BulletTraces(new Pool<IBulletTrace>(_bulletTraceFactory), new TimerFactory(_rayWorkTime, _rays));
