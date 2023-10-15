4e52735dd62966b8dae016e73cbedc29f196c57a
xXdatelXx <artemgurv@gmail.com>
2023-10-15 21:15:00 +0200

    Added fade panel system

---
+++ Assets/Source/Toolkit/Components/UI/Panels/FadePanel.cs
@@ -0,0 +1,42 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace FPS.Toolkit
+{
+    [RequireComponent(typeof(CanvasGroup))]
+    public class FadePanel : MonoBehaviour, IFadePanel
+    {
+        [field: SerializeField] public float FadeTime { get; private set; }
+        [SerializeField] private OnStart _onStart;
+        private CanvasGroup _canvasGroup;
+
+        private void Awake()
+        {
+            _canvasGroup = GetComponent<CanvasGroup>();
+
+            if (_onStart is not OnStart.Null)
+                Fade((int)--_onStart);
+        }
+
+        public void FadeIn() => Fade(1);
+
+        public void FadeOut() => Fade(0);
+
+        private void Fade(int endValue)
+        {
+            _canvasGroup.DOFade(Mathf.Abs(1 - endValue), 0);
+            _canvasGroup.DOFade(endValue, FadeTime);
+
+            bool visible = endValue == 1;
+            _canvasGroup.interactable = visible;
+            _canvasGroup.blocksRaycasts = visible;
+        }
+
+        private enum OnStart
+        {
+            Null,
+            FadeOut,
+            FadeIn
+        }
+    }
+}
---
+++ Assets/Source/Toolkit/Components/UI/Panels/FadePanelWithAnimation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace FPS.Toolkit
+{
+    [RequireComponent(typeof(FadePanel))]
+    public sealed class FadePanelWithAnimation : SerializedMonoBehaviour
+    {
+        [SerializeField] private AsyncTimer _timerToFadeIn;
+        [SerializeField] private AsyncTimer _timerToFadeOut;
+        private IFadePanel _fadePanel;
+
+        private void Awake()
+        {
+            _fadePanel = GetComponent<FadePanel>();
+            StartAnimationLoop().Forget();
+        }
+
+        private async UniTaskVoid StartAnimationLoop()
+        {
+            while (true)
+            {
+                await Animate(_timerToFadeIn, () => _fadePanel.FadeIn());
+                await Animate(_timerToFadeOut, () => _fadePanel.FadeOut());
+            }
+        }
+
+        private static async UniTask Animate(ITimer timer, Action animation)
+        {
+            timer.Play();
+            await timer.End();
+
+            animation.Invoke();
+        }
+    }
+}
